c38efe20071d83938d2b9bd98de267c9f584d64c
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2022-03-05 21:48:40 +0500

    Initial commit

---
+++ src/com/company/prime/PrimeMain.java
@@ -0,0 +1,31 @@
+package com.company.prime;
+
+import java.util.Scanner;
+import java.lang.System;
+/**
+ * @author is Nabeel Tariq Bhatti
+ * @created at 3/5/2022
+ * @Time at 9:34 PM
+ */
+//check if a number is a prime number 
+public class PrimeMain {
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("%s", "enter the number");
+        number = scanner.nextInt();
+        int c = 2;
+
+        while (c<=number){
+            if(number%c==0){
+                System.out.println("prime");
+            }
+            c++;
+
+        }
+    }
+
+
+
+}
