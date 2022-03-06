f3342e6455ead906dbbdd3621ffce46f95446e10
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2022-03-06 22:56:46 +0500

    binary search recursively OrderAgnosticBinarySearch

---
+++ src/com/company/binarysearch/OrderAgnosticBinarySearch.java
@@ -0,0 +1,30 @@
+package com.company.binarysearch;
+
+import java.util.Arrays;
+
+/**
+ * @author is Nabeel Tariq Bhatti
+ * @created at 3/6/2022
+ * @Time at 10:40 PM
+ */
+public class OrderAgnosticBinarySearch {
+    public static int binarySearch(int[] array, int l, int r,int target){
+        int mid = l+(r-l)/2;
+        if(array[mid] == target ){
+            return mid;
+        }else if(target < array[mid] && array[mid] >= array[mid+1]){
+            return binarySearch(array,mid+1,r,target);
+        }else if(target < array[mid] && array[mid] <= array[mid+1]){
+            return binarySearch(array,l,mid-1,target);
+        }else if(target > array[mid] && array[mid] <= array[mid+1]){
+            return binarySearch(array,mid+1,r,target);
+        }else if(target > array[mid] && array[mid] >= array[mid+1]){
+            return binarySearch(array,l,mid-1,target);
+        }
+       return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{1,2,3,4,5,10},0,6,10));
+    }
+}
--- src/com/company/binarysearch/SearchNumberInAnArray.java
+++ src/com/company/binarysearch/SearchNumberInAnArray.java
@@ -1,16 +1,18 @@
-package com.company.linearsearch;
+package com.company.binarysearch;
 
 /**
  * @author is Nabeel Tariq Bhatti
  * @created at 3/6/2022
  * @Time at 10:01 PM
  */
+
+//descending ascending
 public class SearchNumberInAnArray {
     public static int binarySearch(int[] array, int l, int r,int target){
         int mid = l+(r-l)/2;
         if(array[mid] == target ){
             return mid;
-        }else if(array[mid] < target){
+        }else if(target < array[mid]){
             return binarySearch(array,mid+1,r,target);
         }else if(l>r){
             return -1;
@@ -21,7 +23,7 @@ public class SearchNumberInAnArray {
     }
 
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[]{1,3,4,5,10,11,},0,6,10));
+        System.out.println(binarySearch(new int[]{12,11,10,8,7,6},0,6,10));
     }
 
 }
